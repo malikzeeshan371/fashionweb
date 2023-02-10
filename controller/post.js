59ad7a59f3b0471d25df17d20453b324e0e543de
hunny-nik <106017090+hunny-nik@users.noreply.github.com>
2023-02-10 12:28:45 +0500

    done bro

--- controller/post.js
+++ controller/post.js
@@ -5,19 +5,41 @@ const JWT_secret = 'fashionwebsite';
 
 //////////////////////////////////////////////////////////////////////////////////////
 
-///////////// request to get all post public /////////////
+///////// request to get all post public /////////////
 exports.getPostpublic = (req, res) => {
 
-    ////////////////// select all post ///////////
-    pool.query("SELECT * FROM post where status=?", ["public"],(err, rows) => {
-      if (!err) {
-        res.json(rows)
-      } else {
-        return res.status(403).json("invalid error");
-      }
-    })
-  
+  //   ////////////////// select all post ///////////
+  let final = [];
+  pool.query("SELECT p.*, u.full_name AS post_auther  FROM post AS p JOIN user AS u ON (u.id = p.user_id) WHERE p.status = ?", ["public"], async (err, rows) => {
+    if (!err) {
+
+      await rows.forEach(element => {
+        let postid = element.id
+        let postss = {};
+        postss = element
+        pool.query("SELECT c.id,c.comment,c.user_id, u.full_name AS comment_auther  FROM posts_comments AS c JOIN user AS u ON (u.id = c.user_id) where post_id=?", [postid], async (err, com) => {
+          if (!err) {
+            Object.assign(postss, { "comments": com });
+            final.push(postss)
+            // res.json(final)
+          } else {
+            return res.status(403).json(err);
+          }
+        })
+      })
+      setTimeout(() => {
+        res.json(final)
+      }, 4000)
+
+    } else {
+      return res.status(403).json(err);
+    }
+
+  })
 }
+
+//////////////////////////////////////////////////////////////////////////////////////
+
 ///////////// request to get all post friends /////////////
 exports.getPostfriends = (req, res) => {
   ///////////// verify that user is logedin /////////
@@ -28,18 +50,33 @@ exports.getPostfriends = (req, res) => {
     if (err) return res.status(403).json("Token is not valid!");
 
     ////////////////// select all post ///////////
-    pool.query(`SELECT p.*, u.id AS userId, full_name FROM post AS p JOIN user AS u ON (u.id = p.user_id)
-    LEFT JOIN follower AS f ON (p.user_id = f.followed_user_id) WHERE f.following_user_id= ? OR p.user_id =?
-    ORDER BY p.date_time DESC`,[userInfo.id,userInfo.id], (err, rows) => {
+    let final = [];
+    pool.query(`SELECT p.*, u.id AS userId, full_name as post_auther FROM post AS p JOIN user AS u ON (u.id = p.user_id)
+    LEFT JOIN follower AS f ON (p.user_id = f.followed_user_id) WHERE f.following_user_id= ? OR p.user_id =? ORDER BY p.date_time DESC`, [userInfo.id, userInfo.id], async (err, rows) => {
       if (!err) {
-        res.json(rows)
+        await rows.forEach(element => {
+          let postid = element.id
+          let postss = {};
+          postss = element
+          pool.query("SELECT c.id,c.comment,c.user_id, u.full_name AS comment_auther  FROM posts_comments AS c JOIN user AS u ON (u.id = c.user_id) where post_id=?", [postid], async (err, com) => {
+            if (!err) {
+              Object.assign(postss, { "comments": com });
+              final.push(postss)
+              // res.json(final)
+            } else {
+              return res.status(403).json(err);
+            }
+          })
+        })
+        setTimeout(() => {
+          res.json(final)
+        }, 4000)
       } else {
         return res.status(403).json(err);
       }
     })
   })
 }
-
 ///////////////////////////////////////////////////////////////////////////////
 
 ///////////// request to add post /////////////
